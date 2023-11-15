f59051c6df05ccc175b9fa972f808c4bf571b5d3
Krzysztof Smogorzewski <krzysztof.smogorzewski@chaosgears.com>
2023-11-15 19:56:23 +0100

    terraform plan, terraform apply ci/cd

--- versions.tf
+++ versions.tf
@@ -20,6 +20,6 @@ terraform {
 
 provider "aws" {
   region = "eu-central-1"
-  access_key = secrets.AWS_ACCESS_KEY_ID
-  secret_key = secrets.AWS_SECRET_ACCESS_KEY
+  access_key = "${ secrets.AWS_ACCESS_KEY_ID }"
+  secret_key = "${ secrets.AWS_SECRET_ACCESS_KEY }"
 }
